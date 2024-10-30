666bb93677200c79f5252e598f295336a132d571
escurse <av1967@naver.com>
2024-10-30 12:20:11 +0900

    bbs

--- src/main/java/com/escass/advancedbbs/services/BoardSerivce.java
+++ src/main/java/com/escass/advancedbbs/services/BoardSerivce.java
@@ -1,14 +1,23 @@
 package com.escass.advancedbbs.services;
 
 import com.escass.advancedbbs.entities.BoardEntity;
+import com.escass.advancedbbs.mappers.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BoardSerivce {
+    private final BoardMapper boardMapper;
+
+    @Autowired
+    public BoardSerivce(BoardMapper boardMapper) {
+        this.boardMapper = boardMapper;
+    }
+
     public BoardEntity getBoard(String id) {
         if (id == null || id.isEmpty() || id.length() > 10) {
             return null;
         }
-        return null;
+        return this.boardMapper.selectBoardById(id);
     }
 }
