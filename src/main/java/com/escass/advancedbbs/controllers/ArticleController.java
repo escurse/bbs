6df12ae9c47ce6756f93b95e5fbafec2ee1f7b8b
escurse <av1967@naver.com>
2024-10-30 16:29:21 +0900

    bbs

---
+++ src/main/java/com/escass/advancedbbs/controllers/ArticleController.java
@@ -0,0 +1,31 @@
+package com.escass.advancedbbs.controllers;
+
+import com.escass.advancedbbs.entities.BoardEntity;
+import com.escass.advancedbbs.services.BoardSerivce;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value="/article")
+public class ArticleController {
+    private final BoardSerivce boardSerivce;
+
+    @Autowired
+    public ArticleController(BoardSerivce boardSerivce) {
+        this.boardSerivce = boardSerivce;
+    }
+
+    @RequestMapping(value="/write", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getWrite(@RequestParam(value= "boardId", required = false) String boardId) {
+        BoardEntity board = this.boardSerivce.getBoard(boardId);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("board", board);
+        modelAndView.setViewName("article/write");
+        return modelAndView;
+    }
+}
