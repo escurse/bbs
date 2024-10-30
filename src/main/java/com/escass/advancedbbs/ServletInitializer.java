f5845d388f2245ce27a5139e02fff259e60e1711
escurse <av1967@naver.com>
2024-10-30 10:22:04 +0900

    bbs

---
+++ src/main/java/com/escass/advancedbbs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.escass.advancedbbs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdvancedBbsApplication.class);
+    }
+
+}
