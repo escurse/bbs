ce87da809c6308281ad47a68a1d836715e94621f
escurse <av1967@naver.com>
2024-10-30 14:07:34 +0900

    bbs

--- src/main/java/com/escass/advancedbbs/controllers/BoardController.java
+++ src/main/java/com/escass/advancedbbs/controllers/BoardController.java
@@ -1,5 +1,8 @@
 package com.escass.advancedbbs.controllers;
 
+import com.escass.advancedbbs.entities.BoardEntity;
+import com.escass.advancedbbs.services.BoardSerivce;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value ="/board")
 public class BoardController {
+    private final BoardSerivce boardSerivce;
+
+    @Autowired
+    public BoardController(BoardSerivce boardSerivce) {
+        this.boardSerivce = boardSerivce;
+    }
+
     @RequestMapping(value ="/list", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
     public ModelAndView getList(@RequestParam(value = "id", required = false) String id) {
+        BoardEntity board = this.boardSerivce.getBoard(id);
+        if (board == null) {
+            System.out.println("그런 게시판 없음");
+        } else {
+            System.out.println("그런 게시판 있음: " +board.getText());
+        }
+
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("board/list");
         return modelAndView;
