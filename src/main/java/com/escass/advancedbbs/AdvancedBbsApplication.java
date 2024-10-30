80d7cba202bcfe641590c7c465fb952c2aa7f044
escurse <av1967@naver.com>
2024-10-30 10:22:11 +0900

    bbs

---
+++ src/main/java/com/escass/advancedbbs/AdvancedBbsApplication.java
@@ -0,0 +1,13 @@
+package com.escass.advancedbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/escass/advancedbbs/AdvancedBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escass.advancedbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
