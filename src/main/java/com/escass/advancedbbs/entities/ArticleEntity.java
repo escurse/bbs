ce3aba3536a01cdd3c3c3134d67ce91b158a838a
escurse <av1967@naver.com>
2024-10-31 12:34:00 +0900

    bbs

--- src/main/java/com/escass/advancedbbs/entities/ArticleEntity.java
+++ src/main/java/com/escass/advancedbbs/entities/ArticleEntity.java
@@ -1,5 +1,23 @@
 package com.escass.advancedbbs.entities;
 
-public class ArticleEntity {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"index"})
+public class ArticleEntity {
+    private int index;
+    private String board_id;
+    private String nickname;
+    private String password;
+    private String title;
+    private String content;
+    private int view;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 }
