13fa41555ddf0cc43391d5d8b2f57f4d823bab03
escurse <av1967@naver.com>
2024-11-04 12:24:11 +0900

    bbs

--- src/main/resources/static/article/assets/scripts/read.js
+++ src/main/resources/static/article/assets/scripts/read.js
@@ -1,16 +1,23 @@
-const $form = document.getElementById('passwordDialog')
+const $cover = document.getElementById('cover');
+const $passwordDialog = document.getElementById('passwordDialog');
+const $main = document.getElementById('main');
 
-$form.onsubmit = (e) => {
-    e.preventDefault();
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState !== XMLHttpRequest.DONE) {
-            return;
+{
+    const $modifyButton = $main.querySelector('button[name="modify"]');
+    const $deleteButton = $main.querySelector('button[name="delete"]');
+    $passwordDialog.onsubmit = (e) => {
+        e.preventDefault();
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) {
+                return;
+            }
+            if (xhr.status < 200 || xhr.status >= 300) {
+                alert("오류 발생")
+                return;
+            }
         }
-        if (xhr.status < 200 || xhr.status >= 300) {
-            return;
-        }
+        xhr.open('GET', location.href);
+        xhr.send();
     }
-    xhr.open('GET', location.href);
-    xhr.send();
 }
