4e08dad9130cec698fa346555732812780277981
escurse <av1967@naver.com>
2024-11-04 12:35:23 +0900

    bbs

--- src/main/resources/static/article/assets/scripts/read.js
+++ src/main/resources/static/article/assets/scripts/read.js
@@ -5,19 +5,17 @@ const $main = document.getElementById('main');
 {
     const $modifyButton = $main.querySelector('button[name="modify"]');
     const $deleteButton = $main.querySelector('button[name="delete"]');
-    $passwordDialog.onsubmit = (e) => {
-        e.preventDefault();
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE) {
-                return;
-            }
-            if (xhr.status < 200 || xhr.status >= 300) {
-                alert("오류 발생")
-                return;
-            }
-        }
-        xhr.open('GET', location.href);
-        xhr.send();
+    $deleteButton.onclick = () => {
+        $cover.classList.add('--visible');
+        $passwordDialog['mode'].value = 'delete';
+        $passwordDialog['password'].value = '';
+        $passwordDialog.classList.add('--visible');
+    };
+}
+
+{
+    $passwordDialog['cancel'].onclick = () => {
+        $cover.classList.remove('--visible');
+        $passwordDialog.classList.remove('--visible');
     }
 }
