89342d85a50b5189f038f59148eedfec7cae31b3
escurse <av1967@naver.com>
2024-11-11 08:26:59 +0900

    bbs

--- bbs.sql
+++ bbs.sql
@@ -26,3 +26,33 @@ CREATE TABLE `advanced_bbs`.`articles`
 );
 
 DROP TABLE `advanced_bbs`.`articles`;
+
+CREATE TABLE `advanced_bbs`.`images`
+(
+    `index`        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `data`         LONGBLOB     NOT NULL,
+    `content_type` VARCHAR(50)  NOT NULL,
+    `name`         VARCHAR(250) NOT NULL,
+    `created_at`   DATETIME     NOT NULL DEFAULT NOW(),
+    CONSTRAINT PRIMARY KEY (`index`)
+);
+
+CREATE TABLE `advanced_bbs`.`comments`
+(
+    `index`         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `article_index` INT UNSIGNED NOT NULL,
+    `comment_index` INT UNSIGNED NULL     DEFAULT NULL,
+    `nickname`      VARCHAR(10)  NOT NULL,
+    `password`      VARCHAR(128) NOT NULL,
+    `content`       VARCHAR(100) NOT NULL,
+    `created_at`    DATETIME     NOT NULL DEFAULT NOW(),
+    `updated_at`    DATETIME     NULL     DEFAULT NULL,
+    `deleted_at`    DATETIME     NULL     DEFAULT NULL,
+    CONSTRAINT PRIMARY KEY (`index`),
+    CONSTRAINT FOREIGN KEY (`article_index`) REFERENCES `advanced_bbs`.`articles` (`index`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    CONSTRAINT FOREIGN KEY (`comment_index`) REFERENCES `advanced_bbs`.`comments` (`index`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
